5ddbaf75c658613e1504dd790172bae8d770b81b
Stan van de Wiel <stan.vande.wiel@eleo.tech>
2025-01-02 10:29:25 +0100

    Updated workflows

--- Update-Manifest.ps1
+++ Update-Manifest.ps1
@@ -5,18 +5,26 @@ param (
     [version] $ModuleVersion 
 )
 
+$manifestPath = "$Module\$Module.psd1"
+
+$OldModuleManifest = Import-PowerShellDataFile $manifestPath
+
 $publicFunctions = @()
 Get-ChildItem -Path "$Module\Public\*.ps1" -Recurse -ErrorAction SilentlyContinue | ForEach-Object {
     $publicFunctions += $_.BaseName
 }
 
 $params = @{
-    Path                 = "$Module\$Module.psd1"
-    FunctionsToExport    = $publicFunctions
+    Path              = $manifestPath
+    FunctionsToExport = $publicFunctions
 }
 
 if ($PSBoundParameters.ContainsKey('ModuleVersion')) {
+    if ([version]$ModuleVersion -le [version]$OldModuleManifest.ModuleVersion) {
+        return $false
+    }
     $params.ModuleVersion = $ModuleVersion
 }
 
 Update-ModuleManifest @params
+return $true
