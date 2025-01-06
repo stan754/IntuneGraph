04172bcf0c530fde7fcb0d15d330a24185120387
stan754 <stanvdwiel754@gmail.com>
2025-01-06 16:57:38 +0100

    Update GenerateDocs.ps1

--- Tools/GenerateDocs.ps1
+++ Tools/GenerateDocs.ps1
@@ -229,4 +229,8 @@ $Public | ForEach-Object {
   New-MarkdownDoc -CommandName $_.BaseName -OutputFolder "$ReferenceDocsPath\Public" -Tags "Public"
 }
 
-"nav:`n`t- Public`n`t-Private" | Out-File "$ReferenceDocsPath\.pages"
+@"
+nav:
+  - Public
+  - Private
+"@ | Out-File "$ReferenceDocsPath\.pages"
